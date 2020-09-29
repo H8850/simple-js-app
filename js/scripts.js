b36127256ac2fa41338531230adaf3575740e4fa
H8850 <70569109+H8850@users.noreply.github.com>
2020-09-29 15:46:23 +0200

    added object placeholder

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-//second task javascript
-let pokemonList = [];
+//second task
+let pokemonList = [
+  {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison']};
+  {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison']};
+  {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison']};
+];
