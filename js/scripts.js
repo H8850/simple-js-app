7bbfe67e34ec60ab941cd2dc910f1d329e6ed6b9
H8850 <70569109+H8850@users.noreply.github.com>
2020-09-29 15:49:55 +0200

    entered object data

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //second task
 let pokemonList = [
   {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison']};
-  {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison']};
-  {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison']};
+  {name: 'Charmander', height: 6, type: ['fire']};
+  {name: 'Squirtle', height: 5, type: ['water']};
+  {name: 'Caterpie', height: 3, type: ['bug']};
 ];
