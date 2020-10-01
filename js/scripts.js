818689eab2c2d504079120d873125844d4968d08
H8850 <70569109+H8850@users.noreply.github.com>
2020-10-01 10:00:40 +0200

    added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,12 @@ let pokemonList = [
   {name: 'Charmander', height: 6, type: ['Fire']};
   {name: 'Squirtle', height: 5, type: ['Water']};
   {name: 'Caterpie', height: 3, type: ['Bug']};
-  {name: 'Weedle', height: 3, type: ['Bug', 'Poison']}
+  {name: 'Weedle', height: 3, type: ['Bug', 'Poison']};
+  {name: 'Pidgey', height: 3, type: ['Flying', 'Normal']};
+  {name: 'Rattata', height: 3, type: ['Normal']};
+  {name: 'Ekans', height: 2, type: ['Poison']};
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList.name)
+}
