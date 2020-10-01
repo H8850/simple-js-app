5a015e341ff7935e47c1157c2775cf8038756f37
H8850 <70569109+H8850@users.noreply.github.com>
2020-10-01 10:47:43 +0200

    added docwrite for pokemon height

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //second task
 let pokemonList = [
-  {name: 'Bulbasaur', height: 7, type: ['Grass', 'Poison']},
-  {name: 'Charmander', height: 6, type: ['Fire']},
-  {name: 'Squirtle', height: 5, type: ['Water']},
-  {name: 'Caterpie', height: 3, type: ['Bug']},
-  {name: 'Weedle', height: 3, type: ['Bug', 'Poison']},
-  {name: 'Pidgey', height: 3, type: ['Flying', 'Normal']},
-  {name: 'Rattata', height: 3, type: ['Normal']},
-  {name: 'Ekans', height: 2, type: ['Poison']},
+  {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+  {name: 'Charmander', height: 0.6, type: ['Fire']},
+  {name: 'Squirtle', height: 0.5, type: ['Water']},
+  {name: 'Caterpie', height: 0.3, type: ['Bug']},
+  {name: 'Weedle', height: 0.3, type: ['Bug', 'Poison']},
+  {name: 'Pidgey', height: 0.3, type: ['Flying', 'Normal']},
+  {name: 'Rattata', height: 0.3, type: ['Normal']},
+  {name: 'Ekans', height: 0.2, type: ['Poison']},
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ("<br />") );
+  document.write(pokemonList[i].name + ", " + "meters tall."+("<br />") );
 }
