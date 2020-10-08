0d7de1cb2339611d37d4678ade0a311d864eecec
H8850 <70569109+H8850@users.noreply.github.com>
2020-10-08 11:03:13 +0200

    added new function + comments to explain code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 
+//Main Group
 let pokemonRepository = (function () {
+  //Array for pokemon data
   let repository = [
     {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
     {name: 'Charmander', height: 0.6, type: ['Fire']},
@@ -11,7 +13,7 @@ let pokemonRepository = (function () {
     {name: 'Ekans', height: 0.2, type: ['Poison']},
     {name: 'Moltres', height: 2, type: ['Fire', 'Flying']},
   ];
-
+  //Function to add additional pokemon
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
@@ -23,34 +25,40 @@ let pokemonRepository = (function () {
       console.log("pokemon is not correct");
     }
   }
-
+  // function to get the complete pokemon list
   function getAll() {
     return repository;
   }
-
+  //function to create list of buttons
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listPokemon = document.createElement("li");
+    // adding pokemon buttons
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
+    //adding a listener to each button
+    let button = document.querySelector("button");
+    button.addEventListener("click", showDetails (pokemon) {
+      console.log(pokemon)
+    )};
   }
-
+  //function for event listener on each pokemon button
+  function showDetails(pokemon) {
+   console.log(pokemon);
+  }
+  // returns
   return {
     add:add,
     getAll: getAll,
     addListItem: addListItem,
-
   };
-
 })();
-
 pokemonRepository.add({ name: "Pikachu", height: 0.3, type: ["Electric"]});
-
 console.log(pokemonRepository.getAll());
-
+// forEach loop that calls the complete pokemon list
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
